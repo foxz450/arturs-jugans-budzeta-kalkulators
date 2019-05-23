c60987ffeb33895da4ffadf1d4e50a2fbad488fe
Arturs Jugans <johndoe@example.coarturs.20997@Gmail.com>
2019-05-23 09:08:44 +0300

    first comit

---
+++ Budzetu-kalkulators-master/mainJS.js
@@ -0,0 +1,19 @@
+document.getElementById("income");//1.Nodevinēt mainīgos kurš iegūst ienākumu saraksta elemetu no html dokumenta
+document.getElementById("expense");//2.Nodevinēt mainīgos kurš iegūst izdevumu saraksta elemetu no html dokumenta
+document.getElementById("total_income");//3.Nodevinēt mainīgos kurš iegūst pieejamā budžeta vertības elemetu no html dokumenta
+
+function mainitajs(total_income){//4.Izveidot funkciju_1, kas izmaina pieejamā budžeta vertību
+
+function pievienot(add_value){//5.Izveidot funkciju_2, kuru izsaucot, tiek pievienoti ienākumi vai izdevumi html elementu sarakstiem
+
+function poga(myfunction){//6.Izveidot funkciju_3, kas izsaucas pēc pogas "pievienot" nospiešanas
+if //7.Funkcijai_3 jāpārbauda vai apraksta un summas lauks ir aizpildīts, ja nav, tad izvada paziņojumu par to, ka kāds no laukiem nav aizpildīts
+//8.Funkcijai_3 ir jāizsauc funkcija_2, kas pievieno ienākumu vai izdevumu sarakstam jaunu ierakstu.
+
+//9.Lai pārbaudītu vai izveidotās funkcijas darbojas pareizi, izsauc tās zemāk un ievieto fake datus
+//10.Izsaukt funkciju_2, kas pievieno ienākumu sarakstam vertības
+//11.Izsaukt funkciju_2, kas pievieno izdevumu sarakstam vertības
+//12.Izsaukt funkciju_1, kas izmaina pieejamā budžeta vertību
+}
+}
+}
